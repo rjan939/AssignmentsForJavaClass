e6fbb06b6d718f37052632f17d1fa55cc7eeb6e7
rjan <dg47gamer@gmail.com>
2023-07-02 17:32:41 -0500

    first version

--- src/ECommerceFinalProject/Book.java
+++ src/ECommerceFinalProject/Book.java
@@ -1,33 +1,55 @@
 package ECommerceFinalProject;
+import java.util.ArrayList;
+
 
 public class Book {
-    private String ISBN;
-    private String title;
-    private String author;
-    private double cost;
-    private String category;
-    private String[] reviews;
-    public Book(String isbnInput, String titleInput, String authorInput, double costInput,
-                String categoryInput, String[] reviewsInput) {
+    private ArrayList<String> authors;
+    public String ISBN;
+    public String title;
+    public double cost;
+    public String category;
+    public String[] reviews;
+    public Book(String isbnInput, String titleInput, double costInput,
+                String categoryInput, String[] reviewsInput, String authorInputOne,
+                String authorInputTwo, String authorInputThree, String authorInputFour,
+                String authorInputFive, String authorInputSix, String authorInputSeven, String authorInputEight,
+                String authorInputNine, String authorInputTen) {
+        this.authors = new ArrayList<>();
         this.ISBN = isbnInput;
         this.title = titleInput;
-        this.author = authorInput;
         this.cost = costInput;
         this.category = categoryInput;
         this.reviews = reviewsInput;
+        authors.add(authorInputOne);
+        authors.add(authorInputTwo);
+        authors.add(authorInputThree);
+        authors.add(authorInputFour);
+        authors.add(authorInputFive);
+        authors.add(authorInputSix);
+        authors.add(authorInputSeven);
+        authors.add(authorInputEight);
+        authors.add(authorInputNine);
+        authors.add(authorInputTen);
     }
 
     public Book(String commaSeparatedLine) {
+        authors = new ArrayList<>();
         String[] informationAboutBook = commaSeparatedLine.split(", ");
         this.ISBN = informationAboutBook[0];
         this.title = informationAboutBook[1];
-        this.author = informationAboutBook[2];
+        //this.authors = informationAboutBook[2];
         this.cost = Double.parseDouble(informationAboutBook[3]);
+
     }
 
     public void printBookDetails() {
-        System.out.println("ISBN Number: " + ISBN + ", Title: " + title + ", Author(s): " + author
+        System.out.println("ISBN Number: " + ISBN + ", Title: " + title
                 + ", Cost: " + cost + ", Category: " + category);
+        for (String current : authors) {
+            if (!current.equals("")) {
+                System.out.println("Author: " + current);
+            }
+        }
         System.out.println("***********REVIEWS***********");
         for (String review : reviews) {
             System.out.println(review);
--- src/ECommerceFinalProject/Cart.java
+++ src/ECommerceFinalProject/Cart.java
@@ -1,4 +1,26 @@
 package ECommerceFinalProject;
 
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Book> books;
+
+    public Cart() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book titleOfBook) {
+        books.add(titleOfBook);
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (Book book : books) {
+            total += book.cost;
+        }
+        return total;
+    }
+
+
+
 }
